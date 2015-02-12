895df87a2976f94f7e1d4771daea57ef835c908c
Pablo Vallejo <pabloninety@gmail.com>
2015-02-12 12:26:57 -0500

    Bump Version

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the delightful' + chalk.red('DjangoAxiacore') + ' generator!'
+      'Welcome to the delightful ' + chalk.red('DjangoAxiacore') + ' generator!'
     ));
 
     var prompts = [{
